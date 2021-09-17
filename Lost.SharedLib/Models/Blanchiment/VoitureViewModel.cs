ec48d40dc7405d39ea3f3a2306119fa3f582dcea
dbellenger <dbellenger@assystem.com>
2021-09-17 17:10:55 +0200

    Voiture update

--- Lost.SharedLib/Models/Blanchiment/VoitureViewModel.cs
+++ Lost.SharedLib/Models/Blanchiment/VoitureViewModel.cs
@@ -1,14 +1,42 @@
-﻿namespace Lost.SharedLib
+﻿using System;
+
+namespace Lost.SharedLib
 {
     public class VoitureViewModel : BaseViewModel
     {
+        public delegate void TypeVoitureEventHandler(object sender, TypeVoitureEventArgs e);
+        public event TypeVoitureEventHandler TypeVoitureEvent;
+
         public PersonneViewModel PersonneViewModel { get; set; }
 
-        public string TypeVoiture { get; set; }
+        private string typeVoiture { get; set; }
+        public string TypeVoiture {
+            get
+            {
+                return typeVoiture;
+            }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    value = null;
+                }
+                if(typeVoiture != value)
+                {
+                    typeVoiture = value;
+                    TypeVoitureEvent?.Invoke(this, new TypeVoitureEventArgs() { TypeVoiture = value });
+                }
+            }
+        }
 
         public VoitureViewModel()
         {
 
         }
+    }
+
+    public class TypeVoitureEventArgs : EventArgs
+    {
+        public string TypeVoiture { get; set; }
     }
 }
